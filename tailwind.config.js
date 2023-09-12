4265367224cc2d62ba598dc192c88c8c411da35b
alemran <emran309@gmail.com>
2023-09-12 22:53:12 +0600

    assignment 3

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-primary": "#f85559",
+        "brand-secondary": "#121212",
+        text: "rgba(18, 18, 18, 0.60)",
+      },
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
